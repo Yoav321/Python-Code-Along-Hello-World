24b27fbdf3ca574d009c88131be6a8802660ae39
Yoav321 <yoav.bierkatz@gmail.com>
2026-08-24 09:12:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code Along: Hello World
+In this code along activity, you will follow your instructor to write a program that says "Hello World!" on the console. It will then be enhanced to ask the user for their name and greet them by name.
---
+++ hello_world.py
@@ -0,0 +1,12 @@
+"""
+This is a first program to output Hello World to the console. It will then
+be enhanced to ask the user for their name and greet them by name.
+First Last - Month Year
+"""
+
+def main() -> None:
+    pass
+
+# main guard
+if __name__ == "__main__":
+    main()
